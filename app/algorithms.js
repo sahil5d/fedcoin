9cf9a589f79ec7731ebe8bca7452d78667f292b2
Sahil Gupta <sahil.gupta1729@gmail.com>
2017-04-22 16:51:21 -0400

    update concept of nickname, decide on rsa library

--- app/algorithms.js
+++ app/algorithms.js
@@ -4,18 +4,23 @@
 
 /*
 Notes
+pk is public key
+sk is secret private key
 
+todo = to do now
+future = to do later
 */
 
 const crypto = require('crypto');
-// todo need an elliptic curve package
+const nodeRSA = require('node-rsa');
 
 const secrets = require('./secrets');
 const codes = secrets.codes;
 
-// todo: data structure
-// map NODECLASS.NICKNAME (NODE) to NODECLASS
-var nicknameMap = {};
+// todo
+// key is NODE == NODECLASS.PK
+// value is NODECLASS
+var getNodeClass = {};
 
 
 // return array of nodes
@@ -35,16 +40,16 @@ class Vote {
 
 class Addrid {
 	constructor(tx, index, value) {
-		this.tx = tx;
-		this.index = index;					// index(address) in tx output
+		this.txdigest = tx.digest;			
+		this.index = index;		// index(address) in tx output
 		this.value = value;
-		this.digest = crypto.createHmac('sha256', codes.first)
-						.update(tx+index+value)
-						.digest('hex');		// hmac hashes key with tx,indx,val
+		this.digest = crypto.createHash('sha256')
+						.update(tx.digest + index + value)
+						.digest('hex');
 	}
 
 	toString() {
-		return this.tx + this.index + this.value;
+		return this.txdigest + this.index + this.value;
 	}
 }
 
@@ -54,27 +59,32 @@ class Tx {
 		this.inputs = inputs;	// array of addrids
 		this.outputs = outputs;	// array of addrids
 		this.value = value;
+		this.digest = crypto.createHash('sha256')
+						.update(JSON.stringify(inputs+outputs+value))
+						.digest('hex');
 	}
-
-	// get value() { return this.value; }
-	// set value(x) { this.value = x; }
 }
 
-// NODECLASS is the class doing tx verification == the actual bank
-// NODE is what the public sees == NODECLASS.NICKNAME == bank stock symbol
+// NODECLASS is the class doing tx verif, is the commercial bank
+// NODE is what users call node's public key PK
 class NodeClass {
 	constructor(nickname, utxo, pset, txset) {
-		this.nickname = nickname;	// what users calls the 'node'
-									// must be unique among nodeClasses
-		this.pk = crypto.createHmac('sha256', codes.second)
-						.update(nickname)
-						.digest('hex');		// public key
-											// todo can't just make pk, need sk
+		this.nickname = nickname;	// could be bank stock symbol
+		this.sk = 5;				// todo sk. keep secure
+		this.pk = 5; 				// todo something involving
+		// this.address = null;		// future for central bank to pay reward
 		this.utxo = utxo;			// object of unspent tx outputs
-									// key is ADDRID.DIGEST, value is null
-									// but value is [address, value] if spent
-		this.pset = pset;			// for catching double spends
-		this.txset = txset;			// for sealing txs
+									// key is ADDRID.DIGEST, val true=unspent
+		this.pset = pset;			// object of txs to catch double spending
+									// key is ADDRID.DIGEST, val is tx
+		this.txset = txset;			// array for sealing txs
+	}
+
+	// todo
+	checkTx(tx) {
+		// total input val >= total output value
+		// input addrids point to valid txs
+		// sigs authorizing prev tx outputs are valid
 	}
 
 	// algorithm v.2
@@ -84,7 +94,8 @@ class NodeClass {
 		var digest = addrid.digest;
 
 		return new Promise((resolve, reject) => {
-			if (!this.checkTx(tx)||!getOwners(addrid).includes(this.nickname)){
+			if (!this.checkTx(tx) ||
+				!getOwners(addrid).includes(this.pk)) { // todo refactor to whether this node !HAS the addrid
 				resolve(null);
 			} else if (this.utxo[digest] || this.pset[digest] == tx) {
 				this.utxo[digest] = null;	// idempotent action
@@ -97,37 +108,57 @@ class NodeClass {
 	}
 
 	// todo
-	checkTx(tx) {
-		// total input val >= total output value
-		// input addrids point to valid txs
-		// sigs authorizing prev tx outputs are valid
-	}
-
-	// todo
 	// algorithm v.3
 	// input transaction TX, period J, BUNDLE
 	// return promise of node's vote
 	commitTx(tx, j, bundle) {
+		return new Promise((resolve, reject) => {
+			var addridSample = tx.outputs[0];
 
+			if (!this.checkTx(tx) ||
+				!getOwners(addridSample).includes(this.pk)) { // todo refactor to whether this node !HAS the addridsample
+				resolve(null);
+			} else {
+				var allInputsValid = true;
+				// for loop todo
+
+
+					var nVotesOwners = null;
+					// for loop todo
+
+				if (!allInputsValid)
+					resolve(null);
+				else {
+					for (var i in tx.outputs) {
+						var addrid = tx.outputs[i];
+						this.utxo[addrid.digest] = true;
+					}
+					this.txset.push(tx);
+					// todo resolve
+				}
+			}
+		});
 	}
 }
 
 
 class User {
-	// todo refactor this constructor
+	// todo refactor this constructor. need array of privkey,pubkey,address
+	// need constructor where can instantiate with passphrase
 	constructor(prop1, prop2) {
 		this.prop1 = prop1;
 		this.prop2 = prop2;
 	}
 
-	// helper fxs to find NODECLASS with nickname NODE
+	// helper fxs to find NODECLASS from NODE
 	// return promise of NODECLASS's vote
 	static checkUnspent(node, addrid, tx) {
-		var nodeClass = nicknameMap[node];
+		var nodeClass = getNodeClass[node];	// todo replace with https request
+											// shouldn't have access to nodecls
 		return nodeClass.checkUnspent(addrid, tx);
 	}
 	static commitTx(node, tx, j, bundle) {
-		var nodeClass = nicknameMap[node];
+		var nodeClass = getNodeClass[node];	// todo replace with https request
 		return nodeClass.commitTx(tx, j, bundle);
 	}
 
@@ -173,7 +204,7 @@ class User {
 		}
 		
 		// wait for all queries to finish
-		// todo: put time limit on how long Promise.all waits
+		// future: put time limit on how long Promise.all waits
 		Promise.all(queries).then(results => {
 			// an array of nulls, votes, or errors
 			console.log('queries results ' + results);
@@ -200,9 +231,9 @@ class User {
 			}
 
 			// wait for all commits to finish
-			// todo: put time limit on how long Promise.all waits
+			// future: put time limit on how long Promise.all waits
 			Promise.all(commits).then(results => {
-				// note: user can save this array to audit node in future
+				// note: user can save this array to audit node later
 				// an array of nulls, votes, or errors
 				console.log('commits results ' + results);
 			}).catch(err => {
